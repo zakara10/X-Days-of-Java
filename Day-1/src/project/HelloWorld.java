fc18d59c64622d5051fb51cfb095430717d42966
Roshan Kumar <roshan10kushwaha@gmail.com>
2021-03-05 00:37:44 +0530

    Add day 2 programs, polygon print program

--- Day-1/src/project/HelloWorld.java
+++ Day-1/src/project/HelloWorld.java
@@ -2,6 +2,6 @@ package project;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World \n ");
+        System.out.print("Hello World \n");
     }
 }
